de3efb87f1d94bf62c34fbc011805d1c84a8eeb5
Amit23kum <43128621+Amit23kum@users.noreply.github.com>
2018-09-10 23:20:24 -0700

    need to see if i am running this task properly

---
+++ needguide.py
@@ -0,0 +1,12 @@
+#Doctor: a function that adds the "Doctor" title to a name
+#Define function make_doctor()  that takes a parameter name
+#get user input for variable full_name
+#call the function using full_name   as argument
+#print the return value
+# create and call make_doctor() with full_name argument from user input - then print the return value
+
+
+def make_doctor():
+    Full_name=input("patient full name: ")
+    return Full_name
+print(make_doctor())
